786814403c3a4b1671b873afb19066798f812188
PhillipCostas <Phillipcostas@gmail.com>
2024-05-21 14:08:50 -0400

    try button working

--- app.js
+++ app.js
@@ -9,11 +9,11 @@ const colorButtons = document.querySelectorAll('.color')
 const start = document.querySelector('#start')
 const restart = document.querySelector('#restart')
 
-let highScore = false 
-let loser = false 
+let highScore = false
+let loser = false
 
 
-let colorsArray = ['green' , 'red' , 'blue' , 'yellow']
+let colorsArray = ['green', 'red', 'blue', 'yellow']
 let currentIndex = 0
 let colorOptions = []
 let playerChoices = colorOptions
@@ -23,7 +23,6 @@ let currentColor = colorOptions
 
 /*------------------------ Cached Element References ------------------------*/
 const play = document.querySelectorAll('#button')
-
 
 
 
@@ -38,8 +37,6 @@ const play = document.querySelectorAll('#button')
 
 /*-------------------------------- Functions --------------------------------*/
 
-
-
 const randomColor = () => {
     let randomIndex = [Math.floor(Math.random() * colorsArray.length)]
     const color = colorsArray[randomIndex]
@@ -47,39 +44,42 @@ const randomColor = () => {
     console.log(color)
 }
 
+
 const handleColorChoice = (event) => {
     const currentColor = event.target.id
     console.log(`'playerChoice' ${playerChoices}`)
- 
+
     if (currentColor === colorOptions[currentIndex]) {
         console.log(`'color match' ${currentColor}`)
-        // currentIndex = currentIndex.length = 0
-        currentIndex += 1; 
-        
+        currentIndex += 1;
+
         console.log(currentIndex)
-    if (currentIndex === colorOptions.length) {
-        randomColor()
-        currentIndex = 0
-    }
-       
-    } else { 
+        if (currentIndex === colorOptions.length) {
+            randomColor()
+            currentIndex = 0
+        }
+    } else {
         console.log('color not a match')
-        loser = true 
-            return console.log("You lose! :-(")
+        loser = true
+        return console.log("You lose! :-(")
+        //Give player the option to restart the Game! 
     }
 }
-
+const tryAgain = () => {
     
+}
 
-const startGame = () => { 
+const startGame = () => {
+    currentIndex = 0 
+    currentColor = 0
+    colorOptions = []
     randomColor()
 }
 
-start.addEventListener('click' , startGame)
+start.addEventListener('click', startGame)
 for (let i = 0; i < colorButtons.length; i++) {
     colorButtons[i].addEventListener('click', handleColorChoice)
 }
 
-
-
-
+tryAgain()
+restart.addEventListener('click' , startGame)
