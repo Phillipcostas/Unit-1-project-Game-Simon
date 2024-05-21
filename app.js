9081836890ff3d4620d5eb6e1589fe7f7b1e1ef7
PhillipCostas <Phillipcostas@gmail.com>
2024-05-21 11:23:06 -0400

    Updated handleColorChoice- loops through choices, user is able to guess correct, if user guesses wrong then returns you loose

--- app.js
+++ app.js
@@ -4,73 +4,78 @@
 
 
 /*-------------------------------- Variables --------------------------------*/
-const colorButtons = document.querySelectorAll('.colors')
+const colorButtons = document.querySelectorAll('.color')
 
 const start = document.querySelector('#start')
 const restart = document.querySelector('#restart')
 
-let winner = false 
+let highScore = false 
 let loser = false 
 
-let sequence = []
-let level = 0;
 
 let colorsArray = ['green' , 'red' , 'blue' , 'yellow']
 let currentIndex = 0
 let colorOptions = []
 let playerChoices = colorOptions
+let currentColor = playerChoices 
+
 
 
 /*------------------------ Cached Element References ------------------------*/
-//const play = document.querySelectorAll('#button')
-//const sequences = document.querySelector('sequence')
+const play = document.querySelectorAll('#button')
+
 
 
 
 
 /*----------------------------- Event Listeners -----------------------------*/
 
-colorButtons.forEach(addEventListener('click' , (e) => {
-    console.log(e.target)
-}));
+// colorButtons.forEach(button => addEventListener('click' , (e) => {
+//    console.log(e.target)
+// }));
 
 
 
 /*-------------------------------- Functions --------------------------------*/
 
+
+
 const randomColor = () => {
     let randomIndex = [Math.floor(Math.random() * colorsArray.length)]
     const color = colorsArray[randomIndex]
-    console.log(color)
     colorOptions.push(color)
+    console.log(color)
 }
-
-const startGame = () => { 
-      randomColor()
-}
-start.addEventListener('click' , startGame)
 
 
 const handleColorChoice = (event) => {
     const currentColor = event.target.id
-
-    for (let i = 0; i < colorButtons.length; i++) {
-        colorButtons[i].addEventListener('click', handleColorChoice)
-
+   
     if (currentColor === colorOptions[currentIndex]) {
+        console.log('color match')
         currentIndex += 1; 
-    
-    if (currentIndex < colorOptions.length )
-        if(currentIndex === colorOptions.length)
-    randomColor()
-        }
+        console.log(currentIndex)
+    if (currentIndex === colorOptions.length || (currentIndex < colorOptions.length ))
+        randomColor()
+        currentIndex = 0
+      
+    } else { 
+        console.log('color not a match')
+        loser = true 
+            return console.log("You lose! :-(")
     }
+}
+    
+
+const startGame = () => { 
+    randomColor()
+}
+
+start.addEventListener('click' , startGame)
+for (let i = 0; i < colorButtons.length; i++) {
+    colorButtons[i].addEventListener('click', handleColorChoice)
 }
 
 
 
 
-
-
-
-//playerChoice.addEventListener('click' , )
