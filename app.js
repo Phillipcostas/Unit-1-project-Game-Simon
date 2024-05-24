b47cb96ce009c5d9f8ef7b039146433ba591cf0e
PhillipCostas <Phillipcostas@gmail.com>
2024-05-24 12:30:31 -0400

    updated the color flashing when the same color has been generated

--- app.js
+++ app.js
@@ -1,17 +1,14 @@
+
+const play = document.querySelectorAll('#button')
 const colorButtons = document.querySelectorAll('.color')
 const colorDisplay = document.querySelector('.colordisplay')
-const start = document.querySelector('#start') 
+const startBtn = document.querySelector('#start')
 const levels = document.querySelector('#level')
 
 
 let highScore = 0
-let highScores = ['1' , '2' , '3']
+let highScores = []
 let loser = false
-
-
-document.getElementById("start").addEventListener("click", function hidebutton() {
-    this.style.display = "none";
-  });
 
 let colorsArray = ['green', 'red', 'blue', 'yellow']
 let currentIndex = 0
@@ -21,11 +18,28 @@ let playerChoices = []
 let currentColor = 0 
 let clearColor = '#00000000'
 
+function setupApplication() {
+    colorDisplay.style.display = "none";
+    startBtn.addEventListener('click', () => {
+        hideButton();
+        colorDisplay.style.display = "block";
+        startGame();
+    });
+    for (let i = 0; i < colorButtons.length; i++) {
+        colorButtons[i].addEventListener('click', handleColorChoice)
+    }
+}
 
-/*------------------------ Cached Element References ------------------------*/
-const play = document.querySelectorAll('#button')
+function hideButton() {
+    startBtn.style.display = "none";
+}
 
-/*-------------------------------- Functions --------------------------------*/
+function showButton(btnText) {
+    startBtn.style.display = "block";
+    if (btnText) {
+        startBtn.textContent = btnText
+    }
+}
 
 const randomColor = () => {
     let randomIndex = Math.floor(Math.random() * colorsArray.length)
@@ -33,14 +47,12 @@ const randomColor = () => {
     colorOptions.push(color)
     console.log(color)
     setTimeout(colorTimer, 500)
-    hideButton()
 }
 
 const handleColorChoice = (event) => {
     const currentColor = event.target.id
     console.log(`'playerChoice' ${playerChoices}`)
-    console.log('hsandle color chiuce', currentIndex, 'high scofe:', highScore)
-
+    console.log('handle color choice', currentIndex, 'high score:', highScore)
 
     if (currentColor === colorOptions[currentIndex]) {
         console.log(`'color match' ${currentColor}`)
@@ -58,74 +70,48 @@ const handleColorChoice = (event) => {
     } else {
         console.log('color not a match')
         loser = true
-        gameOver ()
-        return ("Game Over! Try Again?")
+        gameOver()
     }
-    
 }
-
-const tryAgain = () => {
-    currentIndex = 0 
-    currentColor = 0
-    colorOptions = []
-    levels.innerText = `Level: 1`   
-}
-
 
 const startGame = () => {
     currentIndex = 0 
-    maxIndex = 0
     currentColor = 0
     colorOptions = []
     levels.innerText = `Level: 1`
-    randomColor()
-    hideButton()
-
+    maxIndex = 0
+    randomColor();
 }
 const updateHighScore = () => {
     if (maxIndex > highScore) {
         highScore = maxIndex;
-        console.log(highScore)
-        localStorage.setItem("highScore", highScore);
-        highScores.push(highScore);
+        console.log('highScore', highScore)
+        localStorage.setItem("highScore", `${highScore}`);
+        highScores.push(`${highScore}`);
         localStorage.setItem('highScores', JSON.stringify(highScores));
-        document.getElementById(gameOver, "highScores")
+       
     }
-    localStorage.getItem = ('highscore')
+    const currentHighScore = localStorage.getItem('highScore')
+    console.log('cached value', currentHighScore)
 }
-
-start.addEventListener('click', startGame)
-
-
-for (let i = 0; i < colorButtons.length; i++) {
-    colorButtons[i].addEventListener('click', handleColorChoice)
-}
-
-tryAgain()
 
 const colorTimer = () => {
+    
     for (let i = 0; i < colorOptions.length; i++) {
         setTimeout(() => {
-            colorDisplay.style.background = colorOptions[i];
-        }, i * 750); 
+            colorDisplay.style.background = 'transparent';
+            setTimeout(() => {
+                colorDisplay.style.background = colorOptions[i];
+            }, 200); 
+        }, i * 1000);
     }
-    setTimeout(colorClear, colorOptions.length * 750 + 500)
+    setTimeout(colorClear, colorOptions.length * 1000 + 500);
 };
 
 const colorClear = () => {
        colorDisplay.style.background = clearColor; 
 }
 
-colorTimer();
-colorClear()
-
-const button = document.getElementById("start");
-    function hideButton() {
-    button.style.display = "none";
-    }
-    function showButton() {
-    button.style.display = "block"; 
-}   
 const gameOver = () => {
     if (loser) { 
         updateHighScore()
@@ -134,9 +120,9 @@ const gameOver = () => {
         currentColor = 0
         colorOptions = []
         levels.innerText = `Level: 1`
-        start.textContent = "Game Over!, Try Again?"
     }
+
     showButton("Game Over!! Try Again");
 }
-    button.addEventListener("click", hideButton);
-gameOver() 
+
+setupApplication()
